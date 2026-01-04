314031c93d07815fb29df341ffe55910553a7b05
Xiphiros <72585136+Xiphiros@users.noreply.github.com>
2026-01-04 12:26:18 -0400

    fix(editor): remove unused React import

--- src/web/src/editor/Editor.tsx
+++ src/web/src/editor/Editor.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { EditorProvider, useEditor } from './store/EditorContext';
 import { EditorTimeline } from './components/EditorTimeline';
 import { Playfield } from '../gameplay/components/Playfield';
